eb03a4421cc9302cf2981f95aa7b4f68993c81e6
rosina <rosemat563@gmail.com>
2024-03-05 16:09:27 +0000

    adding password verification

---
+++ password.php
@@ -0,0 +1,22 @@
+<?php
+
+class Password
+{
+    public string $password;
+    public function __construct(string $password)
+    {
+        $uppercase = str_contains('[A-Z]', $password);
+        $number    = str_contains('[0-9]', $password);
+
+        if(strlen($password) < 8 && !$uppercase && !$number) {
+            echo 'Password should be at least 8 characters in length
+             and should include at least one upper case letter and one number';
+        } else {
+            $this->password = $password;
+        }
+    }
+}
+
+
+new Password('HeGRGllo');
+
