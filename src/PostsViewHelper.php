f31f2fc524c906a2d107620ca3fe0a715b41ed0b
Alex Livingston <aslivingston@hotmail.co.uk>
2024-03-05 11:20:34 +0000

    updates completed following code review

---
+++ src/PostsViewHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace src;
+
+require_once 'src/Models/PostModel.php';
+
+class PostsViewHelper
+{
+
+    public function displayAllPosts(array $posts) :void
+    {
+        foreach ($posts as $post) {
+            echo "<article class='p-8 border border-solid rounded-md'>
+                 <div class='flex justify-between items-center flex-col md:flex-row mb-4'>
+                <h2 class='text-4xl'> {$post->title} </h2>
+                </div>
+                <p class='text-2xl mb-2'> {$post->dateTime} - {$post->authorName} </p>";
+            echo "<p>" . mb_strimwidth($post->content, 0, 100, '...') . "</p>
+                </article>";
+        }
+    }
+
+}
