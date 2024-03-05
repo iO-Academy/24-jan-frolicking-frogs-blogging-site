ae4ff460568d1c839fba1e6cc7ffa5cf22fd06fa
Alex Livingston <aslivingston@hotmail.co.uk>
2024-03-05 16:18:38 +0000

    updated log in verify section

--- login.php
+++ login.php
@@ -4,7 +4,35 @@ require_once 'connectToDB.php';
 require_once 'src/Models/UsersModel.php';
 
 session_start();
+function verifyLogin()
+{
+    if (isset($_POST['username'])) {
 
+        $inputtedUsername = $_POST['username'];
+        $inputtedPassword = $_POST['password'];
+
+        $hashedPassword = password_hash($inputtedPassword, PASSWORD_BCRYPT);
+
+        $db = connectToDB();
+        $usersModel = new UsersModel($db);
+
+        $users = $usersModel->selectUser($inputtedUsername);
+        if ($users === null) {
+            echo 'User does not exist';
+        } else {
+            $storedPassword = $users->password;
+            $storedUsername = $users->username;
+
+            if ((password_verify($storedPassword, $hashedPassword)) && ($inputtedUsername === $storedUsername)) {
+                $_SESSION['userid'] = $users->id;
+                $_SESSION['username'] = $users->username;
+                header('Location: index.php');
+            } else {
+                echo 'Sorry, your username or password is incorrect';
+            }
+        }
+    }
+}
 
 verifyLogin();
 
