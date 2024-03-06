7612c33feba245f21e20b61212d13d4876de346b
Alex Livingston <aslivingston@hotmail.co.uk>
2024-03-06 14:36:24 +0000

    posts adding to database via addPost method

--- addPost.php
+++ addPost.php
@@ -2,18 +2,23 @@
 
 require_once 'connectToDB.php';
 require_once 'src/Models/PostModel.php';
+require_once 'SessionHandler.php';
+
+session_start();
 
 if (isset($_POST['submit']))
     {
 
     $inputtedTitle = $_POST['title'];
     $inputtedContent = $_POST['content'];
+    $currentUserId = $_SESSION['userid'];
 
     $db = connectToDb();
 
     $postsModel = new PostModel($db);
 
-    $postsModel->addPost($inputtedTitle, $inputtedContent);
+    $postsModel->addPost($inputtedTitle, $inputtedContent, $currentUserId);
+//    header('index.php');
 
 
 //        if(strlen($inputtedTitle) > 30) {
@@ -50,37 +55,34 @@ if (isset($_POST['submit']))
 <nav class="flex justify-between items-center py-5 px-4 mb-10 border-b border-solid">
     <a href="index.php"><h1 class="text-5xl">Blog</h1></a>
     <div class="flex gap-5">
-        <?php if (!isset($_SESSION['userid'])){
-
+        <?php $session = new SessionHandles();
+        if (!$session->checkUserLoggedIn()) {
             echo '<div class="flex gap-5">
         <a href="login.php">Login</a>
-        <a href="register.php">Register</a>
-        </div>'; } ?>
-        <?php if (isset($_SESSION['userid'])){
-
+        <a href="registration.php">Register</a>
+        </div>'; } else {
             echo '<div class="flex gap-5">
-        <a href="addPost.php">Create Post</a>
-        <a href="logout.php">Log out</a>
-        </div>'; } ?>
+        <a href="logout.php">Logout</a>
+        </div>'; }?>
     </div>
 </nav>
 
-<form class="container lg:w-3/4 mx-auto flex flex-col p-8 bg-slate-200">
+<form method="post" class="container lg:w-3/4 mx-auto flex flex-col p-8 bg-slate-200">
     <h2 class="text-3xl mb-4 text-center">Create new post</h2>
 
     <div class="flex flex-col sm:flex-row mb-5 gap-5">
         <div class="w-full sm:w-2/3">
             <label class="mb-3 block" for="title">Title:</label>
-            <input class="w-full px-3 py-2 text-lg" type="text" id="title" />
+            <input class="w-full px-3 py-2 text-lg" name="title" type="text" id="title" />
         </div>
     </div>
 
     <div class="mb-5">
         <label class="mb-3 block" for="content">Content:</label>
-        <textarea class="w-full" id="content" rows="9"></textarea>
+        <textarea class="w-full" name="content" id="content" rows="9"></textarea>
     </div>
 
-    <input class="px-3 py-2 mt-4 text-lg bg-indigo-400 hover:bg-indigo-700 hover:text-white transition inline-block rounded-sm" type="submit" value="Create Post" />
+    <input class="px-3 py-2 mt-4 text-lg bg-indigo-400 hover:bg-indigo-700 hover:text-white transition inline-block rounded-sm" name="submit" type="submit" value="Create Post" />
 </form>
 
 </body>
