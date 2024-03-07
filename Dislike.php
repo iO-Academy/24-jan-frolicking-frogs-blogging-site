dfd68edf5418647fd84100fec617d53a673e8e4c
rosina <rosemat563@gmail.com>
2024-03-07 15:09:08 +0000

    fixed reaction issues

--- Dislike.php
+++ Dislike.php
@@ -1,10 +1,19 @@
 <?php
 
 require_once 'SessionHandler.php';
+require_once 'src/Models/PostModel.php';
+require_once 'connectToDB.php';
 
 session_start();
 
 $session = new SessionHandles();
 if (!$session->checkUserLoggedIn()) {
     header('Location: login.php');
+} else {
+    $postId = $_GET['id'];
+    header("Location: singlePost.php?id={$_GET['id']}");
+    $db = connectToDB();
+    $postModel = new PostModel($db);
+    $userId = $_SESSION['userid'];
+    $postModel->DislikePost($postId, $userId);
 }
--- singlePost.php
+++ singlePost.php
@@ -2,21 +2,12 @@
 
 require_once 'connectToDB.php';
 require_once 'src/Models/PostModel.php';
-require_once 'SessionHandler.php';
 
 $db = connectToDB();
 $postModel = new PostModel($db);
 
 session_start();
 $singlePostDetails = $postModel->getSinglePostById($_GET['id']);
-$session = new SessionHandles();
-
-$postId = $_GET['id'];
-if ($session->checkUserLoggedIn()) {
-    $userId = $_SESSION['userid'];
-} else {
-    $userId = 0;
-}
 
 ?>
 
@@ -44,11 +35,8 @@ if ($session->checkUserLoggedIn()) {
         <p class="text-2xl mb-10"><?php echo $singlePostDetails->dateTime; ?> - By <?php echo $singlePostDetails->authorName; ?></p>
         <p><?php echo $singlePostDetails->content; ?></p>
         <div class="flex justify-center gap-5">
-           <?php echo '<a class="px-3 py-2 mt-4 text-lg bg-green-300 hover:bg-green-400 hover:text-white transition inline-block rounded-sm" href="Like.php">Like</a>';
-           $postModel->LikePost($postId, $userId); ?>
-
-            <?php echo '<a class="px-3 py-2 mt-4 text-lg bg-red-300 hover:bg-red-400 hover:text-white transition inline-block rounded-sm" href="Dislike.php">Dislike </a>';
-//            $postModel->DislikePost($postId, $userId);?>
+           <?php echo "<a class='px-3 py-2 mt-4 text-lg bg-green-300 hover:bg-green-400 hover:text-white transition inline-block rounded-sm' href='Like.php?id={$_GET['id']}'>Like</a>";
+           echo "<a class='px-3 py-2 mt-4 text-lg bg-red-300 hover:bg-red-400 hover:text-white transition inline-block rounded-sm' href='Dislike.php?id={$_GET['id']}'>Dislike</a>"; ?>
         </div>
         <div class="flex justify-center">
             <a class="px-3 py-2 mt-4 text-lg bg-indigo-400 hover:bg-indigo-700 hover:text-white transition inline-block rounded-sm" href="index.php">View all posts</a>
